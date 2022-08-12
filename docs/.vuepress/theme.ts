d391dbca8fc3a85172cad1f4cfadf96e9cf05738
Thibaut Gautier <shanyujuneja@gmail.com>
2022-08-12 05:18:42 +0200

    cleanup

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -3,8 +3,6 @@ import * as navbar from "./navbar";
 import * as sidebar from "./sidebar";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-v2-demo.mrhope.site",
-
   author: {
     name: "Ghast",
     url: "https://ghast.dev",
@@ -14,45 +12,9 @@ export default hopeTheme({
 
   logo: '/images/logo.png',
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "terminalsin/skidfuscator-public-docs",
 
-  docsDir: "demo/src",
-
-  pageInfo: ["Author", "Original", "Category", "Tag", "ReadingTime"],
-
-  blog: {
-    medias: {
-      Baidu: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
-      Email: "https://example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
-      GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "https://example.com",
-      Instagram: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
-      Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
-    },
-  },
+  pageInfo: ["Author", "Original", "ReadingTime"],
 
   locales: {
     "/": {
@@ -79,12 +41,7 @@ export default hopeTheme({
 
       footer: "默认页脚",
 
-      displayFooter: true,
-
-      blog: {
-        description: "一个前端开发者",
-        intro: "/zh/intro.html",
-      },
+      displayFooter: false,
     },
   },
 
